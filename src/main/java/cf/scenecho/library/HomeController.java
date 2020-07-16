4f21c73a2414d243a8e4d9626269102247d2498f
scene <scene_cho@naver.com>
2020-07-16 17:37:27 +0900

    [#1] initial

--- src/main/java/cf/scenecho/library/HomeController.java
+++ src/main/java/cf/scenecho/library/HomeController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String index() {
-        if (true) {
-
-        }
         return "index";
     }
 
